f997a50ae00bb820719ebce15a68edc6e5599f36
sarah-svg <kjskillen@gmail.com>
2020-10-27 20:02:43 -0700

    added one drop down

--- src/App.js
+++ src/App.js
@@ -1,72 +1,22 @@
 
 import React, { Component } from 'react';
-
-import images from './data.js';
+import Header from './Header.js';
+import ImageList from './ImageList.js';
 import './App.css';
-
-
-class Header extends React.Component {
-  state = {
-    filter: ''
-  }
-
-  handleChange = e => {
-    this.setState({
-      filter: e.target.value
-    });
-  }
-  render() {
-   
-      return (
-        <>
-          <div> Hi, Welcome  {this.props.name}, to the Horned Animal Page!</div>
-          <div>Search through who has the most horns.</div>
-          
-          <select onChange={this.handleChange}>
-          <option value=''>All Options</option>
-          <option value='1'>1</option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-        <option value='100'>100</option>
-     </select>
-     </>
-      );
-  }
-}
+import items from './data.js';
 
 
 
-
-
-
-
- class ImageItem extends Component {
-  render() {
-      return (
-          <div className="image-item" stye={{ background: this.props.color }}>
-              <div>{this.props.title}</div>
-              <img className="creature" src={this.props.src} alt='horned creature' />
-              <div>{this.props.description}</div>
-          </div>
-      )
-  }
-}
 
 function App() {
 
   return (
+    <>
     <div className="App">
       <Header name="Sarah" />
-          <div />
-     {
-        images.map(image => <ImageItem
-          title={image.title}
-          src={image.url}
-          description={image.description} />
-        )
-      }
-
+     <ImageList items={items}/>
     </div>
+    </>
   );
 }
 
---
+++ src/ImageItem.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react'
+
+ export default class ImageItem extends Component {
+    render() {
+        return (
+            <>
+            <div className="image-item" stye={{ background: this.props.color }}>
+                <div>{this.props.title}</div>
+                <img className="creature" src={this.props.src} alt='horned creature' />
+                <div>{this.props.description}</div>
+            </div>
+            
+            </>
+        )
+    }
+  }
+  
